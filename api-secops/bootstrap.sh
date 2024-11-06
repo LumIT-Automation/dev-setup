e278fbcc14be62410cb722e1a221cce3966052fc
Marco Buratto <m.buratto@lumit.it>
2024-11-06 12:05:45 +0100

    secOps API: set PrivateTmp to false in apache's service file

--- api-secops/bootstrap.sh
+++ api-secops/bootstrap.sh
@@ -279,6 +279,10 @@ function System_apacheSetup()
     usermod --shell /bin/bash www-data
     printf "$1\n$1" | passwd www-data
 
+    # Set PrivateTmp to false in apache's service file.
+    cp -f /vagrant/api-secops/etc/systemd/system/consul.service /etc/systemd/system/consul.service
+    chmod 644 /etc/systemd/system/consul.service
+
     # Force enabling the wsgi module.
     a2enmod wsgi
 
